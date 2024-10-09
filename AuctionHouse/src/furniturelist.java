937521dc6e35616ecd2e8e228164dc1b15f5f473
antonin310 <antonin.moreno1@outlook.fr>
2024-10-09 01:34:02 +0100

    # Issue 1 : add methods to furniturelist class

--- AuctionHouse/src/furniturelist.java
+++ AuctionHouse/src/furniturelist.java
@@ -8,17 +8,69 @@ public class furniturelist {
     public furniturelist(){
         FurnitureList = new ArrayList<>();
     }
+
     // add a furniture to the lost of furniture
     public void addFurniture(furniture item) {
         FurnitureList.add(item);
     }
 
     // get the total number of furniture in the list
-
     public int get_TotalItem() {
         return FurnitureList.size();
     }
 
-    
+    // get the oldest furniture in the list
+    public furniture getOldestItem() {
+        if (FurnitureList.isEmpty()) return null;
+
+        furniture oldest = FurnitureList.get(0);
+        for (furniture item : FurnitureList) {
+            if (item.get_yearsoforigins() < oldest.get_yearsoforigins()) {
+                oldest = item;
+            }
+        }
+        return oldest;
+    }
+
+    // get the newest furniture in the list 
+    public furniture getNewestItem() {
+        if (FurnitureList.isEmpty()) return null;
+
+        furniture newest = FurnitureList.get(0);
+        for (furniture item : FurnitureList) {
+            if (item.get_yearsoforigins() > newest.get_yearsoforigins()) {
+                newest = item;
+            }
+        }
+        return newest;
+    }
+
+    // get the most exepensive furniture in the list
+    public furniture getMostExpensiveItem() {
+        if (FurnitureList.isEmpty()) return null;
+
+        furniture mostExpensive = FurnitureList.get(0);
+        for (furniture item : FurnitureList) {
+            if (item.get_startingprice() > mostExpensive.get_startingprice()) {
+                mostExpensive = item;
+            }
+        }
+        return mostExpensive;
+    }
+
+    // get the least expensive furniture in the list  
+    public furniture getLeastExpensiveItem() {
+        if (FurnitureList.isEmpty()) return null;
+
+        furniture leastExpensive = FurnitureList.get(0);
+        for (furniture item : FurnitureList) {
+            if (item.get_startingprice() < leastExpensive.get_startingprice()) {
+                leastExpensive = item;
+            }
+        }
+        return leastExpensive;
+    }
+
+
 }
 
