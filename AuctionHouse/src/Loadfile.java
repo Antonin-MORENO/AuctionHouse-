5045a49b8385993a3f01a2ac54d5fd7ab094e222
antonin310 <antonin.moreno1@outlook.fr>
2024-10-22 00:28:33 +0200

    # Issue 3 : add loadfile class to read csv file

---
+++ AuctionHouse/src/Loadfile.java
@@ -0,0 +1,55 @@
+package AuctionHouse.src;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class Loadfile {
+    
+    public static void loadFurnitureFromFile(String filePath, ArrayList<Furniture> furnitureList) {
+
+        
+        try {
+
+            File f = new File(filePath);
+            Scanner scanner = new Scanner(f);
+
+            while (scanner.hasNextLine()) {
+
+
+                String inputLine = scanner.nextLine();
+
+                String[] fields = inputLine.split(",");
+
+                String objectType = fields[0].trim();
+                String style = fields[1].trim();
+                String makerName = fields[2].trim();
+                double length = Double.parseDouble(fields[3].trim());
+                double height = Double.parseDouble(fields[4].trim());
+                double depth = Double.parseDouble(fields[5].trim());
+                int year = Integer.parseInt(fields[6].trim());
+                String ownerName = fields[7].trim();
+                String condition = fields[8].trim();
+                double price = Double.parseDouble(fields[9].trim());
+
+                Furniture furniture = new Furniture(objectType, style, makerName, length, height, depth, year, ownerName, condition, price);
+
+                furnitureList.add(furniture);
+
+                
+            }
+            scanner.close();
+
+        } catch (FileNotFoundException e) {
+            System.out.println("Error : file not found " + e.getMessage());
+        } catch (NumberFormatException e) {
+            System.out.println("Error : wrong format in the file " + e.getMessage());
+        }
+        
+        
+        }
+
+
+
+}
