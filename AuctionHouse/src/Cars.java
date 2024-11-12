9ded19aaa01049148501a38daefe1f3cc2bd7d8e
antonin310 <antonin.moreno1@outlook.fr>
2024-11-12 17:26:40 +0100

    # Issue 4 : add Cars class and its constructor

---
+++ AuctionHouse/src/Cars.java
@@ -0,0 +1,19 @@
+package AuctionHouse.src;
+
+public class Cars extends Items {
+
+    private String make;
+    private String model;
+    private boolean serviced;
+
+    public Cars(String owner_name, String condition, int year_of_origins, double starting_price, String make, String model, boolean serviced){
+        super(owner_name, condition, year_of_origins, starting_price);
+
+        this.make = make;
+        this.model = model;
+        this.serviced = serviced;
+
+    }
+    
+    
+}
