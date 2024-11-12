eea4236a3b68f16c6e3f0177c992b3044e5a643e
antonin310 <antonin.moreno1@outlook.fr>
2024-11-12 18:32:53 +0100

    #Issue 4 : add Books class and its constructor

---
+++ AuctionHouse/src/Books.java
@@ -0,0 +1,21 @@
+package AuctionHouse.src;
+
+public class Books extends Items {
+
+    // Attributes 
+    private String title;
+    private String authorname;
+    private String edition;
+    private String genre;
+
+    public Books(String title, String authorname, String edition, String genre, String owner_name, String condition, int year_of_origins, double starting_price){
+        super(owner_name, condition, year_of_origins, starting_price);
+
+        this.title = title;
+        this.authorname = authorname;
+        this.edition = edition;
+        this.genre = genre;
+
+    }
+    
+}
