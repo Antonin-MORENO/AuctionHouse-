100de4e7ceb4819fe7b74fcb5d8587c8faae9166
antonin310 <antonin.moreno1@outlook.fr>
2024-11-14 19:29:20 +0100

    # Issue 6 : Updated Cars class to include year estimates

--- AuctionHouse/src/Cars.java
+++ AuctionHouse/src/Cars.java
@@ -6,7 +6,7 @@ public class Cars extends Items {
     private String model;
     private boolean serviced;
 
-    public Cars(String owner_name, String condition, int year_of_origins, double starting_price, String make, String model, boolean serviced){
+    public Cars(String owner_name, String condition, YearEstimate year_of_origins, double starting_price, String make, String model, boolean serviced){
         super(owner_name, condition, year_of_origins, starting_price);
 
         this.make = make;
